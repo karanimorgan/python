204f618489fcbdcb06a2280f316271a022cd5f25
karanimorgan <karanishmoko13@gmail.com>
2026-03-04 11:44:46 +0300

    new update

--- functions/keywords.py
+++ functions/keywords.py
@@ -5,3 +5,10 @@ def volume_of_box(length, width, height):
 
 box_dimensions = (10, 5, 2)
 volume_of_box(*box_dimensions)
+
+other_box_dimensions = {
+    "length": 15,
+    "width": 7,
+    "height": 3
+}
+volume_of_box(**other_box_dimensions)
