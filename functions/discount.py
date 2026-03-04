770cf4420c842de7c3be97354a4e6848e0a06734
karanimorgan <karanishmoko13@gmail.com>
2026-03-04 10:26:46 +0300

    created anothr file

--- functions/discount.py
+++ functions/discount.py
@@ -4,6 +4,11 @@ def calculate_discounted_price(price, discount=10):
     return final
 
 if __name__ == "__main__":
-   price = 100
-   discounted_price = calculate_discounted_price(price)
-print(f"The discounted price of {price} with a discount of 10% is: {discounted_price} ")
+    price = 100
+    discounted_price = calculate_discounted_price(price)
+    print(f"The discounted price of {price} with a discount of 10% is: {discounted_price} ")
+
+    price = 200
+    discount = 20
+    discounted_price = calculate_discounted_price(price, discount)
+    print(f"The discounted price of {price}with a discount of 20%is: {discounted_price}")
