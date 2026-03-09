6209520f265248a6e1fcec968f815e895ab264ec
karanimorgan <karanishmoko13@gmail.com>
2026-03-09 10:56:59 +0300

    control structure

---
+++ control_structure/if_statements.py
@@ -0,0 +1,13 @@
+car = "Toyota"
+
+if car == "Toyota":
+#     print("This is a Toyota car.")
+# print("This is not a Toyota")
+    pass
+
+def check_car(car):
+    if car == "Toyota":
+        print("This is a Toyota car.")
+check_car("Toyota")
+
+        
