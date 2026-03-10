0f7d0787072052d5ccf384cc0a92fe3c854a8d9e
karanimorgan <karanishmoko13@gmail.com>
2026-03-10 11:43:34 +0300

    new

--- control_structure/divisible.py
+++ control_structure/divisible.py
@@ -5,10 +5,13 @@ def div (n):
 
     elif  n % 3 == 0:
          print (f"{n}is divisible by 3 ")
+    elif n % 5 == 0:
+        print(f"{n}is divisible by 5")     
 
     else:
         print(f"{n}is not divisible") 
         
 div(10)
 div(7)   
-div(9)    
+div(9) 
+div(25)   
