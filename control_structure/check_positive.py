6d3a909da8e16594cd3c8aaa398c048190ba36db
karanimorgan <karanishmoko13@gmail.com>
2026-03-10 11:29:41 +0300

    tuesday

--- control_structure/check_positive.py
+++ control_structure/check_positive.py
@@ -1,8 +1,11 @@
 def pos_neg_zero(n):
     # check if n is positive negative
-   if n >=0 
-   print(f"positive") 
-   elif n = 0
-   print(f"equal") 
+   if n >0:
+        print(f"{n}is positive") 
+   elif n <0:
+        print(f"{n}is negative") 
    else:
-    print(f"negative")
+        print(f"{n}is zero") 
+pos_neg_zero(50)
+pos_neg_zero(-4)
+pos_neg_zero(0)
--- control_structure/divisible.py
+++ control_structure/divisible.py
@@ -1,11 +1,14 @@
 def div (n):
-    if n / 2 == 0:
-       print (f"{n}is divisible")
+    if n % 2 == 0:
+       print (f"{n}is divisible by 2")
     
 
-    elif  n / 3 == 0:
-         print (f"{n}is divisible")
+    elif  n % 3 == 0:
+         print (f"{n}is divisible by 3 ")
 
     else:
-        printn(f"{n}is not divisible") 
+        print(f"{n}is not divisible") 
         
+div(10)
+div(7)   
+div(9)    
