01365e4711e98d339476974f08f7525aef6e8250
karanimorgan <karanishmoko13@gmail.com>
2026-04-09 10:28:19 +0300

    gg

--- OOP/inheritance/example4.py
+++ OOP/inheritance/example4.py
@@ -3,10 +3,20 @@ class Vehicle:
         self.make = make
         self.model = model
 
-        def display_info(self):
-            print(f"Vehicle: {self.make} {self.model}")
+    def display_info(self):
+        return f"vehicle {self.make} {self.model}"
 
 # create a class car that inherits from vehicle
 # class car will have an additional attribute "year", color
 # and a method 'display_car_info' that displays the cars information including the year and the colour
-    
+class Car(Vehicle):
+    def __init__(self, make, model, year, colour):
+        super().__init__(make, model)
+        self.year = year
+        self.colour = colour
+
+    def display_car_info(self):
+        return f"{super().display_info()},{self.year}, {self.colour}."
+
+car = Car("suzuki", "swift", 2020, "red")
+print(car.display_car_info())
